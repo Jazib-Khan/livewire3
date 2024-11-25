95e7e718a44eb4c503af654533e0804b105e9fcb
Jazib-Khan <jazib1khan@gmail.com>
2024-11-25 19:01:38 +0000

    Using Lifecycle Hooks

--- livewire-app/app/Livewire/Greeter.php
+++ livewire-app/app/Livewire/Greeter.php
@@ -4,12 +4,13 @@ namespace App\Livewire;
 
 use Livewire\Atrributes\Validate;
 use Livewire\Component;
+use App\Models\Greeting;
 
 class Greeter extends Component
 {
     public $name = '';
-
     public $greeting = '';
+    public $greetings = [];
     public $greetingMessage = '';
 
     public function changeGreeting()
@@ -19,6 +20,23 @@ class Greeter extends Component
         $this->validate();
 
         $this->greetingMessage = "{$this->greeting}, {$this->name}!";
+    }
+
+    public function mount()
+    {
+        $this->greetings = Greeting::all();
+    }
+
+    public function updated($property, $value)
+    {
+        /* if ($property === 'name') { */
+        /*     $this->name = strtolower($value); */
+        /* } */
+    }
+
+    public function updatedName($value)
+    {
+        $this->name = strtolower($value);
     }
 
     public function rules()
