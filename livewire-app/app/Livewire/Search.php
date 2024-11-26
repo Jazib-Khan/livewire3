a2c0e5fa4a2e9b75818f1932cb52b936f2b72967
Jazib-Khan <jazib1khan@gmail.com>
2024-11-26 17:55:08 +0000

    Building a Search Component

---
+++ livewire-app/app/Livewire/Search.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Article;
+
+class Search extends Component
+{
+    public $searchText = '';
+    public $results = [];
+
+    public function updatedSearchText($value)
+    {
+        $this->reset('results');
+
+        $this->validate();
+
+        $searchTerm = "%{$value}%";
+
+        $this->results = Article::where('title', 'LIKE', $searchTerm)->get();
+    }
+
+    public function clear()
+    {
+        $this->reset('searchText', 'results');
+    }
+
+    public function rules()
+    {
+        return [
+            'searchText' => 'required',
+        ];
+    }
+
+    public function render()
+    {
+        return view('livewire.search');
+    }
+}
