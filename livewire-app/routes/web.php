8be88d332cc217e284d871898cf3a575765cca11
Jazib-Khan <jazib1khan@gmail.com>
2024-11-26 21:52:28 +0000

    Using Full Page Components

--- livewire-app/routes/web.php
+++ livewire-app/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Search;
+use App\Livewire\ShowArticle;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/search', Search::class);
+
+Route::get('/articles/{article}', ShowArticle::class);
