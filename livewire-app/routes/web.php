0c52b654d6f0121381c360dad8dda77930c2b0cc
Jazib-Khan <jazib1khan@gmail.com>
2024-12-23 13:53:03 +0000

    Using Form Objects

--- livewire-app/routes/web.php
+++ livewire-app/routes/web.php
@@ -6,6 +6,8 @@ use App\Livewire\ShowArticle;
 use App\Livewire\ArticleIndex;
 use App\Livewire\Dashboard;
 use App\Livewire\ArticleList;
+use App\Livewire\CreateArticle;
+use App\Livewire\EditArticle;
 
 Route::get('/', ArticleIndex::class );
 
@@ -14,3 +16,5 @@ Route::get('/', ArticleIndex::class );
 Route::get('/articles/{article}', ShowArticle::class);
 Route::get('/dashboard', Dashboard::class);
 Route::get('/dashboard/articles', ArticleList::class);
+Route::get('/dashboard/articles/create', CreateArticle::class);
+Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
