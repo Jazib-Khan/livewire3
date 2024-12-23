7b77d6d20b7a9e6f5b8e56ae78f0d5bd3fd82107
Jazib-Khan <jazib1khan@gmail.com>
2024-12-23 15:14:41 +0000

    Working with Checkboxes and Radios

--- livewire-app/app/Livewire/Forms/ArticleForm.php
+++ livewire-app/app/Livewire/Forms/ArticleForm.php
@@ -16,9 +16,15 @@ class ArticleForm extends Form
     #[Validate('required')]
     public $content = '';
 
+    public $published = false;
+
+    public $notification = 'none';
+
     public function setArticle(Article $article) {
         $this->title = $article->title;
         $this->content = $article->content;
+        $this->published = $article->published;
+        $this->notification = $article->notification;
 
         $this->article = $article;
     }
@@ -27,7 +33,7 @@ class ArticleForm extends Form
     {
         $this->validate();
 
-        Article::create($this->only(['title', 'content']));
+        Article::create($this->only(['title', 'content', 'published', 'notification']));
     }
 
     public function update()
@@ -35,7 +41,7 @@ class ArticleForm extends Form
         $this->validate();
 
         $this->article->update(
-            $this->only(['title', 'content'])
+            $this->only(['title', 'content', 'published', 'notification'])
         );
     }
 }
