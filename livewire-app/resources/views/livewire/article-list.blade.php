e0e84a258520ddc1ead2eabdeabec3d33c4de0dc
Jazib-Khan <jazib1khan@gmail.com>
2024-12-24 00:41:50 +0000

    Lazy Loading Content

--- livewire-app/resources/views/livewire/article-list.blade.php
+++ livewire-app/resources/views/livewire/article-list.blade.php
@@ -1,5 +1,5 @@
 <div class="m-auto w-1/2 mb-4">
-    <div class="mb-3">
+    <div class="mb-3 flex justify-between items-center">
         <a
             href="/dashboard/articles/create"
             class="text-gray-200 p-2 bg-indigo-700 hover:bg-indigo-900 rounded-sm"
@@ -7,8 +7,9 @@
         >
             Create Article
         </a>
+        <livewire:published-count />
     </div>
-    <table>
+    <table class="w-full">
         <thead class="text-xs uppercase bg-gray-700 text-gray-400">
             <tr>
                 <th class="px-6 py-3">Title</th>
