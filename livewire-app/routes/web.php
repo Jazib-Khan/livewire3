8cf209c4f799d915773902b21d5d1babf3bc08fa
Jazib-Khan <jazib1khan@gmail.com>
2024-12-29 21:23:28 +0000

    Handling Authentication

--- livewire-app/routes/web.php
+++ livewire-app/routes/web.php
@@ -8,13 +8,23 @@ use App\Livewire\Dashboard;
 use App\Livewire\ArticleList;
 use App\Livewire\CreateArticle;
 use App\Livewire\EditArticle;
+use App\Livewire\Login;
 
-Route::get('/', ArticleIndex::class );
+Route::get('/', ArticleIndex::class)->name('home');
+Route::get('/login', Login::class)->name('login');
+Route::get('/logout', function() {
+    Auth::logout();
 
-/* Route::get('/search', Search::class); */
+    return redirect()->route('home');
+});
 
 Route::get('/articles/{article}', ShowArticle::class);
-Route::get('/dashboard', Dashboard::class);
-Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
-Route::get('/dashboard/articles/create', CreateArticle::class);
-Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
+
+Route::middleware([
+    'auth',
+])->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
+    Route::get('/dashboard/articles/create', CreateArticle::class);
+    Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
+});
