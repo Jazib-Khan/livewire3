9777c0dc4e3e9ce4ea59144b6e76a74eefe41aed
Jazib-Khan <jazib1khan@gmail.com>
2024-12-29 16:05:46 +0000

    Redirecting

--- livewire-app/resources/views/livewire/article-list.blade.php
+++ livewire-app/resources/views/livewire/article-list.blade.php
@@ -28,6 +28,12 @@
             </button>
         </div>
     </div>
+
+    @if (session('status'))
+        <div class="text-center bg-green-700 text-gray-200">
+            {{ session('status') }}
+        </div>
+    @endif
     <div class="my-3">
         {{ $this->articles->links() }}
     </div>
