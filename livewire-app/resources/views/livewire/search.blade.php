0a4c87b25ff5bf48fda970143a73d6ec8fb6cccd
Jazib-Khan <jazib1khan@gmail.com>
2024-12-29 20:12:47 +0000

    Offline States

--- livewire-app/resources/views/livewire/search.blade.php
+++ livewire-app/resources/views/livewire/search.blade.php
@@ -5,7 +5,9 @@
                 type="text"
                 class="p-4 w-full border rounded-md bg-gray-700 text-white"
                 wire:model.live.debounce="searchText"
-                placeholder="{{$placeholder}}" />
+                placeholder="{{$placeholder}}"
+                wire:offline.attr="disabled"
+            >
         </div>
     </form>
     <livewire:search-results :results="$results" :show="!empty($searchText)"/>
